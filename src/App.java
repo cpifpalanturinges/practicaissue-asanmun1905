8606ea534769564a54dae75a32fd6d36b8f2eb59
Adrian Sanchez M. <adri1952005.4@gmail.com>
2025-10-24 13:22:36 +0200

    documentacion hola mundo - close #1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!"); // Hola mundo
+        System.out.println("Hello, World!"); // esto es un hola mundo
     }
 }
